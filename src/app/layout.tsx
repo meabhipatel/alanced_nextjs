ab032e1e62c408d3b25b0b22fe0238879c3ddffb
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-25 11:10:29 +0530

    chore added next js in application title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,19 @@ import "./globals.css";
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Alanced",
-	description:
-		"Discover a diverse range of freelance opportunities on Alanced.com. Connect with talented professionals, manage projects efficiently, and grow your business with our secure, user-friendly platform. Join Alanced.com today and find the perfect freelance talent to meet your needs.",
+  title: "Alanced | Next js",
+  description:
+    "Discover a diverse range of freelance opportunities on Alanced.com. Connect with talented professionals, manage projects efficiently, and grow your business with our secure, user-friendly platform. Join Alanced.com today and find the perfect freelance talent to meet your needs.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={poppins.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
 }
