981229129fd9b0369020628510c13d4037162d1f
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-25 12:56:17 +0530

    added signup screen

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="flex h-[80vh] w-full items-center justify-center">
+      <h1 className="text-4xl font-bold">Sing up</h1>
+    </div>
+  );
+};
+
+export default Signup;
