05b7cd797cfac13dd8a4498da8e8fb640042a43f
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-25 12:56:41 +0530

    added search job screen

---
+++ src/app/search-job/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchJob = () => {
+  return (
+    <div className="flex h-[80vh] w-full items-center justify-center">
+      <h1 className="text-4xl font-bold">Search Job</h1>
+    </div>
+  );
+};
+
+export default SearchJob;
