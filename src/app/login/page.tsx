0a566d972a31ab59dc121a031cdcec2a2f627d72
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-25 12:55:30 +0530

    added login screen

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex h-[80vh] w-full items-center justify-center">
+      <h1 className="text-4xl font-bold">Login</h1>
+    </div>
+  );
+};
+
+export default Login;
