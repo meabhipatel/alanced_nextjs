c892258090a95d9b19fabb3698eb42df6a1939e9
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-26 16:14:34 +0530

    configure redux store in application with redux toolkit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import StoreProvider from "./StoreProvider";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <StoreProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </StoreProvider>
       </body>
     </html>
   );
