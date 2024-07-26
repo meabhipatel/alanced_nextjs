91089f780072d038a7d4e980a5fb03ec8a1436f0
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-26 14:44:31 +0530

    feat designed 404 page not found screen

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex h-[70vh] flex-col items-center justify-center">
+      <h2 className="text-2xl font-semibold">Page Not Found</h2>
+      <p className="mt-1 text-gray-500/70">Could not find requested resource</p>
+      <Link
+        href="/"
+        className="mt-3 text-sm text-blue-700/70 hover:text-blue-700 hover:underline"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
