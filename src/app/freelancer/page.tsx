3fa197c1bf1d2a4fb077467b32430d3b155e07db
Sachin Sharma <sachin.wiz91@gmail.com>
2024-07-30 15:05:49 +0530

    create freelance page.

---
+++ src/app/freelancer/page.tsx
@@ -0,0 +1,12 @@
+const Freelance = () => {
+  return (
+    <>
+      <div className="flex h-[80vh] w-full items-center justify-center text-4xl font-semibold">
+        Freelancer Page
+      </div>
+    </>
+  );
+};
+
+export default Freelance;
+;
