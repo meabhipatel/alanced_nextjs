e1643828564f8f844733d1ce8d31852d236fd759
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-30 18:39:51 +0530

    added hirer screen

---
+++ src/app/hirer/page.tsx
@@ -0,0 +1,11 @@
+const Hirer = () => {
+  return (
+    <>
+      <div className="flex h-[80vh] w-full items-center justify-center text-4xl font-semibold">
+        Hirer Page
+      </div>
+    </>
+  );
+};
+
+export default Hirer;
