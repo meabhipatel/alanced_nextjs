e7615a30b8b9f083f72395c069d70418c07e2c61
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-30 14:00:34 +0530

    chore created interface IUserProfile

---
+++ src/interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface IUserProfile {
+  id: number;
+  first_Name: string;
+  last_Name: string;
+  email: string;
+  contact: string;
+  Address: string;
+  images_logo: string;
+  social_media: string;
+  skills: string;
+  about: string;
+  DOB: string | null;
+  gender: string;
+  map: string;
+  experience: number;
+  qualification: string;
+  category: string;
+  Language: string;
+  hourly_rate: number;
+  experience_level: string;
+}
