0bb21c8e25a93393105fce7d6b8dda13493b2d41
Sachin Sharma <sachin.wiz91@gmail.com>
2024-07-31 14:55:44 +0530

    added freelance page

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const Register = () => {
-  return (
-    <>
-      <div className="flex h-[80vh] w-full items-center justify-center text-4xl font-semibold">Register Page</div>
-    </>
-  );
-};
-
-export default Register;
