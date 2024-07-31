e52efae846563930945186fe8eac1af168481ba3
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-07-31 18:51:37 +0530

    chore updated meta data in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,15 @@ import AuthProvider from "@/components/AuthProvider";
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Alanced | Next js",
+  title: {
+    default: "Alanced | Next js",
+    template: "%s - Best platform for freelancer",
+  },
   description:
     "Discover a diverse range of freelance opportunities on Alanced.com. Connect with talented professionals, manage projects efficiently, and grow your business with our secure, user-friendly platform. Join Alanced.com today and find the perfect freelance talent to meet your needs.",
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
