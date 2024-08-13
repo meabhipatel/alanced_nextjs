f0bb6dc3b231d56021b39ef84e2abb78b046b9df
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-08-13 11:33:37 +0530

    chore created separate layout and page component for search job page

--- src/app/(public-routes)/search-job/page.tsx
+++ src/app/(public-routes)/search-job/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import { Metadata } from "next";
 import SearchJob from "./SearchJob";
 
@@ -8,8 +8,19 @@ export const metadata: Metadata = {
     "The best platform to search jobs and projects with ease where we take care of every things which you should avoid for wasting your time unnecessarily.",
 };
 
-const Page = () => {
-  return <SearchJob />;
+interface IProps {
+  searchParams: {
+    q?: string;
+    skills?: string | string[];
+    language?: string | string[];
+    explevel?: string | string[];
+    address?: string | string[];
+    page?: string;
+  };
+}
+
+const Page: FC<IProps> = ({ searchParams }) => {
+  return <SearchJob searchParams={searchParams} />;
 };
 
 export default Page;
