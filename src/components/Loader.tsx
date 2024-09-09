4f7f62d450c1ce3002fb1037f2f34f8f9e3d2fa2
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-09-09 16:16:57 +0530

    feat created loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="mx-auto h-5 w-5 animate-spin rounded-full border-2 border-transparent border-r-white border-t-white" />
+  );
+};
+
+export default Loader;
