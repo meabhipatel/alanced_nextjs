ab4296bd555a93c02b6ac6ac5404c1777dff435e
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-09-10 11:50:10 +0530

    feat update and created generic loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { FC } from "react";
 
-const Loader = () => {
-  return (
-    <div className="mx-auto h-5 w-5 animate-spin rounded-full border-2 border-transparent border-r-white border-t-white" />
-  );
+interface IProps {
+  size?: "sm" | "md" | "lg";
+  color?: "white" | "primary";
+}
+
+const Loader: FC<IProps> = ({ size = "sm", color = "white" }) => {
+  switch (size) {
+    case "sm":
+      return (
+        <div
+          className={`mx-auto h-5 w-5 animate-spin rounded-full border-2 border-transparent ${color === "white" ? "border-r-white border-t-white" : "border-r-[#00D4FF] border-t-[#00D4FF]"}`}
+        />
+      );
+    case "md":
+      return (
+        <div
+          className={`mx-auto h-8 w-8 animate-spin rounded-full border-[2px] border-transparent ${color === "white" ? "border-r-white border-t-white" : "border-r-[#00D4FF] border-t-[#00D4FF]"}`}
+        />
+      );
+    case "lg":
+      return (
+        <div
+          className={`mx-auto h-12 w-12 animate-spin rounded-full border-[3px] border-transparent ${color === "white" ? "border-r-white border-t-white" : "border-r-[#00D4FF] border-t-[#00D4FF]"}`}
+        />
+      );
+  }
 };
 
 export default Loader;
