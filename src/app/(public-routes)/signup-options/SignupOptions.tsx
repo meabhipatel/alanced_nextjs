e4be95608d44df0b3325ccce69d5c918514f560d
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-09-12 12:42:57 +0530

    fix signup-options page jsx.element | void type issue

--- src/app/(public-routes)/signup-options/SignupOptions.tsx
+++ src/app/(public-routes)/signup-options/SignupOptions.tsx
@@ -7,19 +7,21 @@ import logo from "@/assets/images/alanced.png";
 import Image from "next/image";
 import Link from "next/link";
 import { useAppSelector } from "@/store/hooks";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const SignupOptions = () => {
-  const router = useRouter();
   type OptionType = "client" | "freelancer";
   const [selectedOption, setSelectedOption] = useState<OptionType>("client");
-  const { isLoggedIn } = useAppSelector((state) => state.auth);
+  const { isLoggedIn, userType } = useAppSelector((state) => state.auth);
 
   /** ---> If user already Logged in navigating to previous screen. */
   if (isLoggedIn) {
-    return router.back();
+    if (userType === "FREELANCER") {
+      return redirect("/freelancer");
+    } else {
+      return redirect("/hirer");
+    }
   }
-
   return (
     <div
       className="relative flex h-screen bg-cover bg-center px-5"
