edcbea31def7d8d16ae80d0c8ffd475856141f2e
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-09-13 16:34:23 +0530

    feat integrated api in forget password page to send reset password email

--- src/app/(public-routes)/forget-password/page.tsx
+++ src/app/(public-routes)/forget-password/page.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import bgimage from "@/assets/images/reset_password.png";
 import logo from "@/assets/images/alanced.png";
+import toast from "react-hot-toast";
+import { errorLog } from "@/utils/errorLog";
+import { axiosIntance } from "@/utils/axiosIntance";
+import { AxiosError } from "axios";
 
-function ResetPasswordPage() {
+function ForgetPassword() {
+  const [email, setEmail] = useState("");
+
+  const handleForgetPassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!email) {
+      toast.error("Email is Required");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("email", email);
+
+    try {
+      const res = await axiosIntance.post("/account/forgot-password", formData);
+
+      toast.success(res.data.message);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      }
+      errorLog(error);
+    }
+  };
+
   return (
     <div
       className="relative flex h-screen bg-cover bg-center px-5"
@@ -24,7 +53,10 @@ function ResetPasswordPage() {
         </div>
       </Link>
       <div className="flex h-full w-full items-center justify-center">
-        <div className="mx-auto w-full max-w-md border border-blue-300 bg-white p-6 shadow-lg md:max-w-xl md:p-8">
+        <form
+          onSubmit={handleForgetPassword}
+          className="mx-auto w-full max-w-md border border-blue-300 bg-white p-6 shadow-lg md:max-w-xl md:p-8"
+        >
           <h2 className="mb-4 text-center text-xl md:text-2xl">Forget Password</h2>
           <div className="relative mx-auto mb-6 mt-1 w-[150px] md:w-[200px]">
             <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-[#0909E9] to-[#00D4FF] text-center"></div>
@@ -38,15 +70,19 @@ function ResetPasswordPage() {
               type="text"
               placeholder="Email Address"
               className="w-full border-none py-2 pl-4 focus:outline-none"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <button className="w-full rounded-lg bg-gradient-to-r from-[#0909E9] to-[#00D4FF] py-2 text-white transition-colors hover:from-[#00D4FF] hover:to-[#0909E9]">
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-gradient-to-r from-[#0909E9] to-[#00D4FF] py-2 text-white transition-colors hover:from-[#00D4FF] hover:to-[#0909E9]"
+          >
             Submit
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
 }
 
-export default ResetPasswordPage;
+export default ForgetPassword;
