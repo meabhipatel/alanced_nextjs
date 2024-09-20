0c69df4710c1b40258603312d68c16d8d13ced9b
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-09-20 18:02:16 +0530

    feat added empty send proposal screen

---
+++ src/app/freelancer/send-proposal/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SendProposal = () => {
+  return (
+    <div className="flex h-[60vh] w-full items-center justify-center">
+      <h1 className="text-3xl font-semibold">Send Proposal</h1>
+    </div>
+  );
+};
+
+export default SendProposal;
