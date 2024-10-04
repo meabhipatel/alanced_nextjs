42444a6a218f076f64ec5d9cc4bb484101ae592d
Amolica dubey <amolica.dubeywiz91@gmail.com>
2024-10-04 18:03:06 +0530

    update in terms screen

--- src/app/(public-routes)/terms/page.tsx
+++ src/app/(public-routes)/terms/page.tsx
@@ -18,98 +18,100 @@ const TermsPage: React.FC = () => {
           <div className="rounded-lg border-b-2 border-gray-600"></div>
         </div>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          Welcome to Alanced! Before using our platform, please read these Terms &amp; Conditions
-          carefully. By accessing or using Alanced, you agree to be bound by these terms, which
-          govern your use of our website and services.
+          Welcome to Alanced! Please take a moment to review our Terms &amp; Conditions. By
+          continuing to use our platform, you agree to comply with these guidelines, which outline
+          your rights and responsibilities while using our website and services
         </p>
 
         <h1 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
-          User Responsibilities
+          User Obligations
         </h1>
         <ul className="mt-2 list-inside list-disc space-y-2">
-          <li>You must be at least 18 years old to use Alanced.</li>
+          <li>You need to be a minimum of 18 years of age to access Alanced</li>
           <li>
-            You agree to provide accurate and truthful information when creating your account.
+            You commit to submitting accurate and honest information when setting up your account.
           </li>
           <li>
-            You are responsible for maintaining the confidentiality of your account credentials and
-            for all activities that occur under your account.
+            You are accountable for keeping your account credentials secure and for all actions
+            taken under your account
           </li>
           <li>
-            You agree to comply with all applicable laws and regulations when using our platform.
+            You commit to adhering to all relevant laws and regulations while utilizing our
+            platform.{" "}
           </li>
         </ul>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
-          Use of Alanced
+          Utilization of Alanced{" "}
         </h2>
         <ul className="mt-2 list-inside list-disc space-y-2">
           <li>
-            Alanced provides a platform where freelancers and clients can connect for freelance
-            services.
+            Alanced offers a platform that facilitates connections between freelancers and clients
+            seeking freelance services.
           </li>
           <li>
-            You agree to use Alanced solely for lawful purposes and in accordance with these Terms
-            &amp; Conditions.
+            You commit to utilizing Alanced exclusively for legal purposes and in compliance with
+            these Terms and Conditions.
           </li>
-          <li>You are prohibited from using Alanced for any illegal or unauthorized purpose.</li>
+          <li>Using Alanced for any unlawful or unauthorized activities is strictly forbidden.</li>
         </ul>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
-          Payment and Fees
+          Charges and Transactions{" "}
         </h2>
         <ul className="mt-2 list-inside list-disc space-y-2">
-          <li>Alanced may charge fees for certain services provided on the platform.</li>
+          <li>Alanced may impose charges for specific services offered on the platform.</li>
           <li>
-            By using our services, you agree to pay any applicable fees as outlined on the platform.
+            By utilizing our services, you consent to pay any relevant fees as specified on the
+            platform.{" "}
           </li>
-          <li>Payments are processed securely through our designated payment methods.</li>
+          <li>All payments are handled securely via our chosen payment methods.</li>
         </ul>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
-          Intellectual Property
+          Ownership of Intellectual Property{" "}
         </h2>
         <ul className="mt-2 list-inside list-disc space-y-2">
           <li>
-            The content on Alanced, including logos, trademarks, and other proprietary materials, is
-            owned by or licensed to Alanced.
+            The materials found on Alanced, such as logos, trademarks, and other proprietary
+            content, are either owned by Alanced or licensed for its use.
           </li>
           <li>
-            You agree not to use, reproduce, distribute, or create derivative works based on our
-            intellectual property without prior written consent.
+            You acknowledge that you will not use, replicate, distribute, or develop derivative
+            works from our intellectual property without obtaining prior written permission.
           </li>
         </ul>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">Privacy</h2>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          Your privacy is important to us. Please review our Privacy Policy to understand how we
-          collect, use, and disclose your information.
+          We value your privacy greatly. We encourage you to read our Privacy Policy to learn about
+          our practices regarding the collection, use, and sharing of your information.
         </p>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
-          Disclaimer of Warranties
+          Limitations on Guarantees{" "}
         </h2>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          Alanced provides its services on an &quot;as is&quot; and &quot;as available&quot; basis.
-          We do not warrant that our platform will be error-free, secure, or uninterrupted. Your use
-          of Alanced is at your own risk.
+          Alanced offers its services on an &quot;as is&quot; and &quot;as available&quot; basis. We
+          do not guarantee that our platform will be free from errors, secure, or consistently
+          available. Your usage of Alanced is entirely at your own risk.
         </p>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
           Limitation of Liability
         </h2>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          To the extent permitted by law, Alanced shall not be liable for any indirect, incidental,
-          special, consequential, or punitive damages arising out of or in connection with your use
-          of our platform.
+          To the maximum extent allowed by law, Alanced shall not be responsible for any indirect,
+          incidental, special, consequential, or punitive damages resulting from or related to your
+          use of our platform.
         </p>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
           Termination
         </h2>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          Alanced reserves the right to suspend or terminate your access to the platform at any
-          time, with or without cause. Upon termination, your rights to use Alanced will cease
+          Alanced retains the right to suspend or terminate your access to the platform at any time,
+          for any reason or none at all. Once terminated, your rights to utilize Alanced will end
           immediately.
         </p>
 
@@ -117,28 +119,28 @@ const TermsPage: React.FC = () => {
           Governing Law
         </h2>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          These Terms &amp; Conditions shall be governed by and construed in accordance with the
-          laws of the Government Jurisdiction of India, without regard to its conflict of law
-          provisions.
+          These Terms & Conditions will be governed by and interpreted in accordance with the laws
+          of the Government Jurisdiction of India, without considering any conflicting legal
+          principles.
         </p>
 
         <h2 className="font-inter mt-4 text-base font-semibold sm:text-lg md:text-xl">
-          Changes to Terms &amp; Conditions
+          Modifications to Terms & Conditions
         </h2>
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          Alanced reserves the right to modify or update these Terms &amp; Conditions at any time.
-          We will notify you of any changes by posting the revised Terms &amp; Conditions on our
-          website.
+          Alanced retains the authority to amend or revise these Terms & Conditions at any time. We
+          will inform you of any modifications by updating the Terms & Conditions on our website.
         </p>
 
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          By using Alanced, you acknowledge that you have read, understood, and agree to be bound by
-          these Terms &amp; Conditions. If you do not agree with any part of these terms, please do
-          not use our platform.
+          By accessing Alanced, you confirm that you have read, comprehended, and consent to adhere
+          to these Terms & Conditions. If you disagree with any aspect of these terms, we kindly ask
+          that you refrain from using our platform.
         </p>
 
         <p className="text-sm leading-relaxed sm:text-base lg:text-lg">
-          For any questions regarding these Terms &amp; Conditions, please contact us at{" "}
+          If you have any inquiries about these Terms & Conditions, please feel free to reach out to
+          us at{" "}
           <a
             href="mailto:contact@alanced.com"
             className="text-blue-600"
@@ -149,7 +151,7 @@ const TermsPage: React.FC = () => {
         </p>
 
         <p className="mt-4 text-center text-base font-semibold sm:text-lg md:text-xl lg:text-2xl">
-          Thank you for choosing Alanced!
+          We appreciate your choice of Alanced!
         </p>
       </div>
     </div>
