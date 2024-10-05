07c703075eb628c345417dbbe3cc280ef9031db8
Amolica dubey <amolica.dubeywiz91@gmail.com>
2024-10-05 11:51:13 +0530

    update in privacy policy

--- src/app/(public-routes)/privacy-policy/PrivacyPolicy.tsx
+++ src/app/(public-routes)/privacy-policy/PrivacyPolicy.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 const InformationWeCollectContent = () => (
-  <>
+  <Fragment>
     <h1 className="font-inter pt-2 text-left text-lg font-semibold text-[#031136]">
       Information We Collect
     </h1>
@@ -14,46 +14,46 @@ const InformationWeCollectContent = () => (
       <span className="text-[#031136] opacity-50">1.1</span> Account Information
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      When you sign up for an Alanced account, we collect personal information such as your name,
-      email address, username, and password. You may also choose to provide additional details in
-      your profile, such as your location, skills, and a profile picture.
+      When you register for an Alanced account, we collect personal data including your name, email
+      address, username, and password. You may also choose to provide additional profile details,
+      such as your location, skills, and profile picture.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">1.2</span> Payment Information
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      If you use our platform to make or receive payments, we will collect payment information, such
-      as your billing address, credit card number, or other financial details. Please note that we
-      do not store your payment card information on our servers; it is securely processed by our
+      If you use our platform for transactions, we collect payment details, including your billing
+      address, credit card number, or other financial information. Please note that we do not store
+      your payment card information on our servers; all transactions are securely handled by our
       trusted payment processors.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">1.3</span> Communications
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We may collect and store communications between you and other users, such as messages, chat
-      history, and emails sent through our platform.
+      We may collect and retain communications between you and other users, including messages, chat
+      history, and emails exchanged through our platform.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">1.4</span> Usage Data
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We automatically collect information about your interactions with Alanced, including your IP
-      address, browser type, device information, and the pages you visit. We may also use cookies
-      and similar tracking technologies to gather this data.
+      We automatically gather information about your interactions with Alanced, including your IP
+      address, browser type, device details, and the pages you visit. Additionally, we may use
+      cookies and similar tracking technologies to collect this data.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">1.5</span> User Content
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      Any content you create or upload to our platform, including portfolio items, project
-      descriptions, reviews, and messages, is stored on our servers.
+      All content you create or upload on our platform, including portfolio items, project
+      descriptions, reviews, and messages, is securely stored on our servers.
     </p>
-  </>
+  </Fragment>
 );
 
 const HowWeUseYourInformationContent = () => (
-  <>
+  <Fragment>
     <h2 className="font-inter pt-2 text-left text-lg font-semibold text-[#031136]">
       How We Use Your Information
     </h2>
@@ -65,36 +65,36 @@ const HowWeUseYourInformationContent = () => (
       <span className="text-[#031136] opacity-50">2.1</span> Providing Services
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We use your information to facilitate your use of Alanced, including matching freelancers with
-      clients, processing payments, and facilitating communications between users.
+      We use your information to enhance your experience on Alanced, including connecting
+      freelancers with clients, processing payments, and enabling communication between users.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">2.2</span> Improving Services
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We use your data to analyze and improve our services, personalize your experience, and develop
-      new features and functionality.
+      We leverage your data to assess and improve our services, customize your experience, and
+      innovate new features and capabilities.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">2.3</span> Communication
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We may send you notifications, updates, promotional materials, and other communications
-      related to your use of Alanced, as well as for marketing purposes. You can opt out of these
-      communications at any time through your account settings.
+      We may provide you with notifications, updates, promotional materials, and other
+      communications related to your use of Alanced, including for marketing purposes. You have the
+      option to opt out of these communications at any time through your account settings.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">2.4</span> Compliance and Legal Obligations
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We may use your information to comply with legal obligations, resolve disputes, and enforce
-      our Terms of Service.
+      We may process your information to adhere to legal obligations, settle disputes, and ensure
+      compliance with our Terms of Service.
     </p>
-  </>
+  </Fragment>
 );
 
 const SharingYourInformation = () => (
-  <>
+  <Fragment>
     <h2 className="font-inter pt-2 text-left text-lg font-semibold text-[#031136]">
       Sharing Your Information
     </h2>
@@ -106,30 +106,30 @@ const SharingYourInformation = () => (
       <span className="text-[#031136] opacity-50">3.1</span> Service Providers
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We may share your information with third-party service providers who assist us in delivering
-      our services, such as payment processors, customer support providers, and data analytics
-      services.
+      We may disclose your information to third-party service providers that facilitate the
+      provision of our services, such as payment processors, customer support entities, and data
+      analytics firms.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">3.2</span> Legal and Safety
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      We may disclose your information in response to legal requests, to protect the rights and
-      safety of Alanced, our users, or others, or to comply with applicable laws and regulations.
+      We may reveal your information in response to legal inquiries, to safeguard the rights and
+      safety of Alanced, our users, or others, or to adhere to relevant laws and regulations.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">3.3</span> Business Transfers
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      In the event of a merger, acquisition, or sale of assets, your information may be transferred
-      as part of the transaction. We will notify you of any such transfer and any changes to this
-      Privacy Policy.
+      In the event of a merger, acquisition, or disposition of assets, your information may be
+      transferred as part of the transaction. We will notify you of any such transfer and any
+      amendments to this Privacy Policy.
     </p>
-  </>
+  </Fragment>
 );
 
 const YourChoices = () => (
-  <>
+  <Fragment>
     <h2 className="font-inter pt-2 text-left text-lg font-semibold text-[#031136]">Your Choices</h2>
     <div className="relative mb-6 mt-1 w-48">
       <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-[#0909E9] to-[#00D4FF]"></div>
@@ -139,25 +139,26 @@ const YourChoices = () => (
       <span className="text-[#031136] opacity-50">4.1</span> Access and Update Information
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      You can access and update your personal information by logging into your account and editing
-      your profile. If you need assistance, please contact our support team.
+      You can access and modify your personal information by logging into your account and updating
+      your profile. If you require assistance, please reach out to our support team.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">4.2</span> Communication Preferences
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      You can manage your communication preferences by adjusting your settings in your account or
-      unsubscribing from marketing emails. Note that even if you opt out of promotional emails, we
-      may still send you important updates related to your account.
+      You can adjust your communication preferences by changing your settings in your account or by
+      opting out of marketing emails. Please note that, regardless of your choice to unsubscribe
+      from promotional emails, we may continue to send you important updates related to your
+      account.
     </p>
     <h2 className="font-inter text-md cursor-pointer pt-5 text-left font-semibold text-blue-600">
       <span className="text-[#031136] opacity-50">4.3</span> Cookies and Tracking Technologies
     </h2>
     <p className="font-inter text-md pt-2 text-left font-normal text-[#031136] opacity-40">
-      You can manage your cookie preferences through your browser settings. However, disabling
-      cookies may affect your ability to use certain features of our platform.
+      You can adjust your cookie preferences via your browser settings. However, please be aware
+      that disabling cookies may impact your ability to utilize certain features of our platform.
     </p>
-  </>
+  </Fragment>
 );
 
 const PrivacyPolicy = () => {
