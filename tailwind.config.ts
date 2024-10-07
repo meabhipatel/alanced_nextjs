aa7f70d7beaea0869cd65a58d9dbca2086767031
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-10-07 11:14:20 +0530

    update tailwind.config.ts file for container default setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,9 @@ const config: Config = {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
     extend: {
