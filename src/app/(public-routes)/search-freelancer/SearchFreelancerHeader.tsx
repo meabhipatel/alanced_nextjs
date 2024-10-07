c800f964156283dc1d2912b8ef8da977bff26a28
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-10-07 13:01:32 +0530

    chore added container and horizontal padding accroding to break points in search freelnacer page

--- src/app/(public-routes)/search-freelancer/SearchFreelancerHeader.tsx
+++ src/app/(public-routes)/search-freelancer/SearchFreelancerHeader.tsx
@@ -26,7 +26,7 @@ const SearchFreelancerHeader: FC<IProps> = () => {
 
   return (
     <div
-      className="flex h-[50vh] items-end justify-center bg-cover bg-no-repeat pb-8"
+      className="container flex h-[50vh] items-end justify-center bg-red-500 bg-cover bg-no-repeat pb-8 sm:px-5 md:px-10 lg:px-20"
       style={{ backgroundImage: `url(${halfBackground.src})` }}
     >
       <div className="flex w-[95%] rounded-md bg-white p-5 text-2xl sm:w-[80%]">
