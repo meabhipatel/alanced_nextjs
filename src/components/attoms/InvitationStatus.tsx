81239cc384dc65ba0e7e3b7a32fa69d994d7ed89
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-10-10 14:12:51 +0530

    update invitationStatus component and added className props

--- src/components/attoms/InvitationStatus.tsx
+++ src/components/attoms/InvitationStatus.tsx
@@ -3,25 +3,32 @@ import React, { FC, ReactNode } from "react";
 interface IProps {
   status?: "accepted" | "rejected" | "pending";
   children: ReactNode;
+  className?: string;
 }
 
-const InvitationStatus: FC<IProps> = ({ status = "accepted", children }) => {
+const InvitationStatus: FC<IProps> = ({ status = "accepted", children, className }) => {
   switch (status) {
     case "accepted":
       return (
-        <div className="flex h-6 min-w-12 items-center justify-center rounded-full border border-green-500 bg-green-50 px-2 text-sm font-semibold text-green-500">
+        <div
+          className={`flex h-6 min-w-12 items-center justify-center rounded-full border border-green-500 bg-green-50 px-2 text-sm font-semibold text-green-500 ${className}`}
+        >
           {children}
         </div>
       );
     case "rejected":
       return (
-        <div className="flex h-6 min-w-12 items-center justify-center rounded-full border border-red-500 bg-red-50 px-2 text-sm font-semibold text-red-500">
+        <div
+          className={`flex h-6 min-w-12 items-center justify-center rounded-full border border-red-500 bg-red-50 px-2 text-sm font-semibold text-red-500 ${className}`}
+        >
           {children}
         </div>
       );
     case "pending":
       return (
-        <div className="flex h-6 min-w-12 items-center justify-center rounded-full border border-yellow-500 bg-yellow-50 px-2 text-sm font-semibold text-yellow-500">
+        <div
+          className={`flex h-6 min-w-12 items-center justify-center rounded-full border border-yellow-500 bg-yellow-50 px-2 text-sm font-semibold text-yellow-500 ${className}`}
+        >
           {children}
         </div>
       );
