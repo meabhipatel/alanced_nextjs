77332138b480595df8a6e8bc68f56f397cbbcbf0
Amolica dubey <amolica.dubeywiz91@gmail.com>
2024-10-21 12:04:36 +0530

    update in homesection1

--- src/components/FooterSubscribeBox.tsx
+++ src/components/FooterSubscribeBox.tsx
@@ -37,7 +37,7 @@ const FooterSubscribeBox = () => {
     <div className="col-span-2 py-1 md:py-4">
       <div className="-mt-6 w-full bg-[#F4F5F9] p-2">
         <h5 className="ml-[23px] pt-[22px] text-start text-xl font-bold">Subscribe</h5>
-        <div className="mt-5 flex w-full items-center px-5">
+        <div className="mt-5 flex w-full items-center px-5 sm:px-3">
           <input
             type="email"
             className="h-10 w-80 p-2 text-sm focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
