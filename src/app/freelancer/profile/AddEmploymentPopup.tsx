2a7bd911af0543bddec7d853759ef61e3eba9675
Sachin Sharma <sachin.wiz91@gmail.com>
2024-10-22 14:22:46 +0530

    remove commented code.

--- src/app/freelancer/profile/AddEmploymentPopup.tsx
+++ src/app/freelancer/profile/AddEmploymentPopup.tsx
@@ -1,9 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useAppSelector } from "@/store/hooks";
-// import { link } from "react-router-dom";
-// import { AddFreelancerEmploymentAction } from "../../../redux/Freelancer/FreelancerAction";
-// import CategoryList from "@/constant/allSelectionData/CategoryList";
+
 import DesignationList from "@/constant/allSelectionData/DesignationList";
 import { IoClose } from "react-icons/io5";
 import { axiosWithAuth } from "@/utils/axiosWithAuth";
@@ -29,13 +25,6 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
     Company_Leaving_date: "",
   });
 
-  // const accessToken = useSelector(state => state.login.accessToken);
-  //   const accessToken =
-  // useSelector((state) => state.login.accessToken) || localStorage.getItem("jwtToken");
-  //   const addemp = useSelector((state) => state.freelancer.addemp);
-  //   const addemp = useAppSelector((state) => state.auth.addemp);
-  //   const dispatch = useDispatch();
-
   const AddEmploymentData = async () => {
     const formData = new URLSearchParams();
     formData.append("Freelancer_Company_Name", AddEmployment.Freelancer_Company_Name);
@@ -43,15 +32,9 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
     formData.append("Company_Joining_date", AddEmployment.Company_Joining_date);
     formData.append("Company_Leaving_date", AddEmployment.Company_Leaving_date);
 
-    // const empdata = {
-    //   Freelancer_Company_Name: AddEmployment.Freelancer_Company_Name,
-    //   Company_Designation: AddEmployment.Company_Designation,
-    //   Company_Joining_date: AddEmployment.Company_Joining_date,
-    //   Company_Leaving_date: AddEmployment.Company_Leaving_date,
-    // };
     const res = await axiosWithAuth.post("freelance/Add/Freelancer/Employment", formData);
     toast.success(res.data.message);
-    // dispatch(AddFreelancerEmploymentAction(empdata, accessToken));
+
     closeAddEmployment();
   };
 
@@ -73,12 +56,6 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
 
   const [isCurrentlyWorking, setIsCurrentlyWorking] = useState(true);
 
-  //   const formatToYYYYMMDD = (dateStr: String) => {
-  //     if (!dateStr) return "";
-  //     const [day, month, year] = dateStr.split("-");
-  //     return `${year}-${month}-${day}`;
-  //   };
-
   const formatToDDMMYYYY = (dateStr: string) => {
     if (!dateStr) return "";
     const [year, month, day] = dateStr.split("-");
@@ -86,7 +63,6 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
   };
 
   const [cate] = useState(DesignationList);
-  //   console.log(cate, "category");
 
   return (
     <div className="fixed inset-0 z-10 mt-14 overflow-y-auto">
@@ -99,7 +75,6 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
               onClick={closeAddEmployment}
               className="text-gray-500 hover:text-gray-700"
             >
-              {/* <i className="bi bi-x-lg"></i> */}
               <IoClose className="text-3xl" />
             </button>
           </div>
@@ -117,13 +92,12 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
             <h1 className="font-cardo text-left text-[20px] font-normal text-[#031136]">
               Designation
             </h1>
-            {/* <input type="text" className='border mt-2 mb-6 py-1.5 px-2 rounded-md w-full focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600' placeholder='Python Developer' onChange={onChange} name='Company_Designation'/> */}
+
             <select
               onChange={onChange}
               className="mb-6 mt-2 w-full rounded-md border bg-white px-2 py-1.5 focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
               name="Company_Designation"
             >
-              {/* <option disabled selected value="">Role</option> */}
               <option
                 disabled
                 selected
@@ -139,10 +113,6 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
                   {cat}
                 </option>
               ))}
-              {/* <option value="">Select an address</option>
-                    <option value="Indore">Indore</option>
-                    <option value="Ujjain">Ujjain</option>
-                    <option value="Bhopal">Bhopal</option> */}
             </select>
             <div className="flex items-center justify-between">
               <div className="mr-2 flex-1">
@@ -157,14 +127,7 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
                   name="Company_Joining_date"
                 />
               </div>
-              {/* <div className="flex-1 ml-2">
-        <p className="font-cardo text-[18px] text-[#031136] font-normal text-left opacity-50">To</p>
-        <input 
-            type="date" 
-            className='border mt-2 mb-6 py-1.5 px-2 rounded-md w-full focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600' 
-            placeholder='End Date'
-        />
-    </div> */}
+
               <div className="ml-2 flex-1">
                 <p className="font-cardo text-left text-[18px] font-normal text-[#031136] opacity-50">
                   To
@@ -189,7 +152,6 @@ const AddEmploymentPopup: React.FC<IAddEmploymentPopup> = ({ closeAddEmployment 
               />
               <div className="checkbox-border-gradient mr-3 flex h-5 w-5 items-center justify-center rounded bg-transparent">
                 <span className="checkmark hidden">
-                  {/* <i className="bi bi-check-lg pr-2 pt-2"></i> */}
                   <FaCheck className="text-sm" />
                 </span>
               </div>
