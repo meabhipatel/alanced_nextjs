94214b070242dcfd02f228cf8bb010a35dc7b93d
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-10-24 12:28:10 +0530

    fix EditPortfolioPopup components bug and styling issue

--- src/app/freelancer/profile/EditPortfolioPopup.tsx
+++ src/app/freelancer/profile/EditPortfolioPopup.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-// import { Link } from 'react-router-dom';
 import edit from "@/assets/icons/edit.png";
-// import { useSelector } from "react-redux";
-// import axios from "axios";
 import SkillsList from "@/constant/allSelectionData/skillsList";
-// import { toast } from 'react-toastify';
 import CategoryList from "@/constant/allSelectionData/categoryList";
 import toast from "react-hot-toast";
 import Image from "next/image";
@@ -13,40 +9,6 @@ import { axiosWithAuth } from "@/utils/axiosWithAuth";
 import { FreelanceProject } from "./page";
 import { IoClose } from "react-icons/io5";
 
-// interface FreelanceProject {
-//   project_id: number | null;
-//   project_title: string | null;
-//   project_description: string | null;
-//   project_link: string | null;
-//   images_logo: string | null;
-//   project_pdf: string | null;
-//   skills_used: string | null;
-//   category: string | null;
-//   design_by: string | null;
-// }
-
-// interface IProjectDetails {
-//   project_id: number;
-//   project_title: string;
-//   project_description: string;
-//   skills_used: string;
-//   category: string;
-//   project_link: string;
-//   images_logo: File | Blob;
-// }
-
-// interface IProject {
-//   project_id: number;
-//   project_title: string;
-//   project_description: string;
-//   project_link: string;
-//   images_logo: File | Blob | string;
-//   project_pdf: string;
-//   skills_used: string | string[];
-//   category: string;
-//   design_by: string;
-// }
-
 interface IProps {
   closeEditFreeProject: () => void;
   project: FreelanceProject | null;
@@ -54,27 +16,17 @@ interface IProps {
 
 const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, project }) => {
   const id = project && project.project_id;
-  // const accessToken = useSelector(state => state.login.accessToken);
-  //   const accessToken =
-  //     useSelector((state) => state.login.accessToken) || localStorage.getItem("jwtToken");
   const [title, setTitle] = useState((project && project.project_title) || "");
   const [category, setCategory] = useState((project && project.category) || "");
   const [projectLink, setProjectLink] = useState((project && project.project_link) || "");
   const [description, setDescription] = useState((project && project.project_description) || "");
   const [skills, setSkills] = useState<string[]>([]);
-  //   const [currentSkill, setCurrentSkill] = useState("");
   const [error, setError] = useState("");
-
-  //   useEffect(() => {
-  //     if (project.skills_used) {
-  //       setSkills(JSON.parse(project.skills_used.replace(/'/g, '"')));
-  //     }
-  //   }, [project]);
 
   useEffect(() => {
     if (project && project.skills_used) {
       if (typeof project.skills_used === "string") {
-        setSkills(JSON.parse(project.skills_used.replace(/'/g, '"')));
+        setSkills(JSON.parse(project.skills_used.replace(/'/g, '"') ?? "[]"));
       } else if (Array.isArray(project.skills_used)) {
         setSkills(project.skills_used);
       }
@@ -92,21 +44,6 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
 
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
-
-  //   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     let file = null;
-  //     if (e.target.files && e.target.files.length > 0) {
-  //       file = e.target.files[0];
-  //     }
-  //     if (file) {
-  //       const reader = new FileReader();
-  //       reader.onloadend = () => {
-  //         setUploadedImage(reader.result); // set the Data URL for preview purposes
-  //         setUploadedFile(file); // store the File object
-  //       };
-  //       reader.readAsDataURL(file);
-  //     }
-  //   };
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -151,12 +88,6 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
       const res = await axiosWithAuth.put(
         `/freelance/update/Freelancer/Self-project/${id}`,
         formData
-        // {
-        //   headers: {
-        //     Authorization: `Bearer ${""}`,
-        //     "Content-Type": "multipart/form-data",
-        //   },
-        // }
       );
 
       if (res.data.status === 200) {
@@ -222,68 +153,16 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
 
   return (
     <>
-      <style>
-        {`
-    .dropdown-list {
-        border: 1px solid #ccc;
-        max-height: 200px;
-        overflow-y: auto;
-        position: absolute;
-        width: 100%;
-        z-index: 1000;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        background-color: #fff;
-        margin-top:11px;
-    }
-    
-    .dropdown-list li {
-        padding: 10px;
-        cursor: pointer;
-    }
-
-    .dropdown-list li:hover {
-        background-color: #f7f7f7;
-    }
-    `}
-        {`
-    .cat-dropdown-list {
-        border: 1px solid #ccc;
-        max-height: 200px;
-        overflow-y: auto;
-        position: absolute;
-        width: 91%;
-        z-index: 1000;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        background-color: #fff;
-    }
-    
-    .cat-dropdown-list li {
-        padding: 10px;
-        cursor: pointer;
-    }
-
-    .cat-dropdown-list li:hover {
-        background-color: #f7f7f7;
-    }
-    `}
-      </style>
       <div className="fixed inset-0 z-10 mt-24 overflow-y-auto">
         <div className="fixed inset-0 bg-black opacity-50"></div>
         <div className="flex min-h-screen items-center justify-center">
           <div className="relative z-20 w-[90%] rounded-lg bg-white p-6 px-8 md:w-[55%]">
             <div className="flex items-center justify-between">
-              <h1 className="font-cardo text-wrap text-[26px] font-normal text-[#031136]">
-                Edit Portfolio
-              </h1>
+              <h1 className="text-wrap text-[26px] font-normal text-[#031136]">Edit Portfolio</h1>
               <button
                 onClick={closeEditFreeProject}
                 className="text-gray-500 hover:text-gray-700"
               >
-                {/* <i className="bi bi-x-lg"></i> */}
                 <IoClose className="text-3xl" />
               </button>
             </div>
@@ -312,7 +191,7 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                 style={{ display: "none" }}
                 onChange={handleImageUpload}
               />
-              <h1 className="font-cardo text-left text-[20px] font-normal text-[#031136]">Title</h1>
+              <h1 className="text-left text-[20px] font-normal text-[#031136]">Title</h1>
               <input
                 type="text"
                 value={title}
@@ -320,10 +199,11 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                 className="mb-3 w-full rounded-md border px-2 py-1.5 opacity-50 focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
                 placeholder=""
               />
-              <h1 className="font-cardo text-left text-[20px] font-normal text-[#031136]">
-                Category
-              </h1>
-              <div ref={wrapperRef}>
+              <h1 className="text-left text-[20px] font-normal text-[#031136]">Category</h1>
+              <div
+                ref={wrapperRef}
+                className="relative"
+              >
                 <input
                   type="text"
                   value={category}
@@ -337,11 +217,12 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                   placeholder="Select Category"
                 />
                 {isOpen && (
-                  <ul className="cat-dropdown-list">
+                  <ul className="absolute z-50 max-h-[200px] w-full list-none overflow-y-auto border border-[#ccc] bg-white p-0 shadow-lg">
                     {filteredCategories.length > 0 ? (
                       filteredCategories.map((cat, index) => (
                         <li key={index}>
                           <button
+                            className="h-full w-full px-4 py-1 text-start hover:bg-[#f7f7f7]"
                             key={index}
                             onClick={() => {
                               setSearchTerm(cat);
@@ -360,9 +241,7 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                 )}
               </div>
 
-              <h1 className="font-cardo text-left text-[20px] font-normal text-[#031136]">
-                Project Link
-              </h1>
+              <h1 className="text-left text-[20px] font-normal text-[#031136]">Project Link</h1>
               <input
                 type="text"
                 value={projectLink}
@@ -370,9 +249,7 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                 className="mb-3 w-full rounded-md border px-2 py-1.5 opacity-50 focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
                 placeholder=""
               />
-              <h1 className="font-cardo text-left text-[20px] font-normal text-[#031136]">
-                Skills
-              </h1>
+              <h1 className="text-left text-[20px] font-normal text-[#031136]">Skills</h1>
               <div className="my-3 flex flex-wrap items-center rounded-md border p-2">
                 {Array.isArray(skills) &&
                   skills.map((skill, index) => (
@@ -402,11 +279,12 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                     placeholder="Search & Select Skills"
                   />
                   {isOpenSkill && (
-                    <ul className="dropdown-list w-full">
+                    <ul className="absolute z-50 mt-[11px] max-h-[200px] w-full list-none overflow-y-auto border border-[#ccc] bg-white p-0 shadow-lg">
                       {filteredSkills.length > 0 ? (
                         filteredSkills.map((skill, index) => (
                           <li key={index}>
                             <button
+                              className="h-full w-full px-4 py-1 text-start hover:bg-[#f7f7f7]"
                               key={index}
                               onClick={() => {
                                 if (skills.length < 15) {
@@ -430,9 +308,7 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                 </div>
               </div>
               {error && <p className="mt-2 text-red-500">{error}</p>}
-              <h1 className="font-cardo text-left text-[20px] font-normal text-[#031136]">
-                Description
-              </h1>
+              <h1 className="text-left text-[20px] font-normal text-[#031136]">Description</h1>
               <textarea
                 name=""
                 id=""
@@ -443,10 +319,7 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                 onChange={(e) => setDescription(e.target.value)}
               ></textarea>
               <div className="mt-4 flex justify-end">
-                <button
-                  //   to=""
-                  onClick={handleSave}
-                >
+                <button onClick={handleSave}>
                   <span className="mr-3 inline-block rounded border border-none bg-gradient-to-r from-[#0909E9] to-[#00D4FF] px-4 py-[10px] text-sm font-semibold text-white">
                     Save
                   </span>
@@ -455,13 +328,11 @@ const EditFreelancerProjectsPopup: React.FC<IProps> = ({ closeEditFreeProject, p
                   className="inline-block rounded bg-gradient-to-b from-[#0909E9] to-[#00D4FF] p-0.5"
                   onClick={closeEditFreeProject}
                 >
-                  {/* <Link to=""> */}
                   <button className="bg-white px-2 py-1">
                     <p className="from-primary to-danger bg-gradient-to-r bg-clip-text px-[8px] py-[4px] text-sm font-semibold text-transparent">
                       Cancel
                     </p>
                   </button>
-                  {/* </Link> */}
                 </button>
               </div>
             </div>
